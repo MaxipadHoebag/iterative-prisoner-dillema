a875b7fd6bd70b09f8432479982405b31908c4aa
MaxipadHoebag <joseph.howden@ngsd.k12.wi.us>
2014-11-03 09:47:13 -0600

    Player 11

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -286,6 +286,14 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
                 return 'b' # betray is they were sucker last time
             else:
                 return 'c' #otherwise collude
+            if opponent_history[-1]=='c' and history[-1]=='b':
+                return 'b'
+            if opponent_history[-1]=='b' and history[-1]=='c':
+                    return 'b'
+            if opponent_history[-1]=='c' and history[-1]=='c':
+                return 'c'
+                
+            
 
 
 
@@ -388,22 +396,44 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
 
 
 
-
     ######
     ######
     #
     elif player == 11:
-        if getting_team_name:
-            return 'loyal vengeful'
-        else:
-            # use history, opponent_history, score, opponent_score
-            # to compute your strategy
-            if len(opponent_history)==0: #It's the first round: collude
-                return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were severely punished last time
+            if getting_team_name:
+            #if there was a previous round just like 
+                return 'Max, Koner'
             else:
-                return 'c' #otherwise collude
+                # use history, opponent_history, score, opponent_score
+                # to compute your strategy      
+                if len(opponent_history)==0: #It's the first round: collude
+                    return 'c'
+                else:
+                # if there was a previous round just like the last one,
+                # do whatever they did in the round that followed it
+                    recent_round_opponent = opponent_history[-1]
+                    recent_round_me = history[-1]
+                            
+                #go through rounds before that one
+                for round in range(len(history)-1):
+                    prior_round_opponent = opponent_history[round]
+                    prior_round_me = history[round]
+                    #if one matches
+                    if (prior_round_me == recent_round_me) and \
+                            (prior_round_opponent == recent_round_opponent):
+                        return opponent_history[round]
+                # no match found
+                if history[-1]=='c' and opponent_history[-1]=='b':
+                    return 'b' # betray is they were severely punished last time
+                else:
+                    return 'c' #otherwise collude
+                if opponent_history[-2]=='b' and opponent_history[-1]=='c':
+                    return 'b'
+    
+             
+          
+                
+                
 
 
 
